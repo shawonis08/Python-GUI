4f95953840817945c91f253576974f3afecfda4e
shawonis08 <shawon_math07@yahoo.com>
2018-01-03 18:40:53 +0600

    4

--- pyqt_skeleton.py
+++ pyqt_skeleton.py
@@ -1,7 +1,7 @@
 import sys
 from PyQt5 import QtCore, QtGui, uic,QtWidgets
 
-qtCreatorFile = "1.ui"  # Enter file here.
+qtCreatorFile = "2.ui"  # Enter file here.
 
 Ui_MainWindow, QtBaseClass = uic.loadUiType( qtCreatorFile )
 
@@ -11,7 +11,18 @@ class MyApp( QtWidgets.QMainWindow, Ui_MainWindow ):
         QtWidgets.QMainWindow.__init__( self )
         Ui_MainWindow.__init__( self )
         self.setupUi( self )
+        self.pushButton.clicked.connect( self.CalculateTax )
 
+    # def CalculateTax(self):
+    #     price = int( self.price_box.toPlainText() )
+    #     tax = (self.tax_rate.value())
+    #     total_price = price + ((tax / 100) * price)
+    #     total_price_string = "The total price with tax is: " + str( total_price )
+    #     self.results_window.setText( total_price_string )
+
+    def CalculateTax(self):
+        test = self.txtedit.toPlainText()
+        self.txtview.setText(test)
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv )
